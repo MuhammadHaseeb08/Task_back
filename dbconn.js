c3d5e75f8f36256442dfc9b18a2cff00912b03a6
Your Name <you@example.com>
2024-12-12 20:34:55 +0500

    final

--- dbconn.js
+++ dbconn.js
@@ -1,10 +1,5 @@
 const mongoose = require("mongoose");
 
-// Dc1JC2ciMIGbPA10
-// Dc1JC2ciMIGbPA10
-// mongodb+srv://<db_username>:<db_password>@cluster0.d6x3u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-// haseebshabir31
-// 22Z7awlrDXubOkru
 let connection=()=>{
     mongoose
   .connect("mongodb+srv://haseebshabir31:22Z7awlrDXubOkru@cluster0.l3gmc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
